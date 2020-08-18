f0c10de12920aa61a1c16dd20d7bc37edce071ea
wijay <william.jay@colorado.edu>
2020-08-18 10:27:57 -0500

    Add brief documentation to GetMass

--- src/nuchic/EffectiveMass.cc
+++ src/nuchic/EffectiveMass.cc
@@ -8,6 +8,14 @@
 double nuchic::PandharipandePieper::GetMass(const double &mass,
                                             const double &mom,
                                             const double &rho) {
+    /// Computes the effective mass following Pandharipande and Pieper, 
+    /// Phys.Rev.C 45 (1992) 791-798.
+    /// In Eq. (2.6), the effective mass is defined using the group velocity 
+    /// and the single-nucleon optical potential, U(k, rho).
+    /// This function takes the parameterization of U(k, rho) from Eq. (2.16)
+    /// and Eqs (2.17-2.19), which is due to Wiringa.
+    /// The actual expression computed follows from these equations and a few
+    /// lines of easy algebra.
     const double ratio = rho/rho0;
     const double betaTerm = beta*ratio;
     const double lambdaTerm = (lambda[0]+lambda[1]*ratio)*nuchic::Constant::HBARC;
